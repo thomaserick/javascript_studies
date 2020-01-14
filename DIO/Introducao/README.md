9235f85304e56691befc9689754fd4eb9956a3a4
Thomas <thominhaserick@gmail.com>
2020-01-14 12:07:39 -0300

    Function

---
+++ DIO/Introducao/README.md
@@ -0,0 +1,26 @@
+# Curso Digital Innovation One
+
+## Introdução ao JavaScript
+
+### Aprendizado
+
+- alert
+- variáveis
+- array e dicionario
+- condicional (if e else)
+- laço d e repetição while e for
+- push,pop e leght
+- reverse array
+- toString -> array
+- join -> array
+- variavel global / local
+- replace
+- lowerCase , upperCase
+- date
+- function
+- redirecionar page
+- DOM
+- getElement
+- innerHTML
+- onclick
+- onmouseover
--- DIO/Introducao/js/main.js
+++ DIO/Introducao/js/main.js
@@ -12,7 +12,7 @@
 // console.log(frase.toLowerCase());
 // console.log(frase.replace("Japão", "Brasil"));
 
-//*Array
+//* Array
 
 // var lista = ["maça", "banana", "laranja"];
 // lista.push("pera");
@@ -32,10 +32,69 @@
 // console.log(fruta);
 // console.log(fruta.nome);
 
-//Lista de Dicionários
+//*Lista de Dicionários
 
-var frutas = [
-  { nome: "maça", cor: "vermelha" },
-  { nome: "banana", cor: "amarela" }
-];
-console.log(frutas);
+// var frutas = [
+//   { nome: "maça", cor: "vermelha" },
+//   { nome: "banana", cor: "amarela" }
+// ];
+// console.log(frutas);
+
+//*Accept
+// var idade = prompt("Qual sua idade?");
+// if (idade >= 18) {
+//   alert("Maior de idade");
+// } else {
+//   alert("Menor de idade");
+// }
+
+//* Repetições
+
+// var count = 0;
+// while (count <= 5) {
+//   console.log(count);
+//   //count = count + 1;
+//   count++;
+// }
+
+// for (i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+//*Date
+// var d = new Date();
+// //alert(d);
+// console.log("Mês: " + d.getMonth() + 1);
+// console.log("Minuto: " + d.getMinutes());
+// console.log("Dia: " + d.getDay());
+
+//*Function
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+function sub(n1, n2) {
+  return n1 - n2;
+}
+function setReplace(frase, nome, novo_nome) {
+  return frase.replace(nome, novo_nome);
+}
+
+function validaIdade(idade) {
+  var validar;
+
+  if (idade >= 18) {
+    validar = true;
+  } else {
+    validar = false;
+  }
+
+  return validar;
+}
+
+console.log("Soma: " + soma(1, 5));
+console.log("Sub: " + sub(1, 5));
+console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+
+var idade = prompt("Qual sua Idade?");
+console.log(validaIdade(idade));
