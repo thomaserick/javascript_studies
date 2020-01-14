2a407a375c74f56e25d98022a0a473925dbdd8b9
Thomas <thominhaserick@gmail.com>
2020-01-14 10:40:38 -0300

    Introduction

---
+++ DIO/Introducao/js/main.js
@@ -0,0 +1,11 @@
+var nome = "Thomas Erick";
+var n1 = 30;
+var n2 = 20;
+var frase = "Japão é o maior país do mundo";
+//alert(nome + "tem: " + n1 + " anos");
+//alert(n1 + n2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(frase.replace("Japão", "Brasil"));
