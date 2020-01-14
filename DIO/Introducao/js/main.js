b29e0caf8d5b13c97432ac07b6c61fe4149404d5
Thomas <thominhaserick@gmail.com>
2020-01-14 11:33:51 -0300

    Array

--- DIO/Introducao/js/main.js
+++ DIO/Introducao/js/main.js
@@ -1,11 +1,41 @@
-var nome = "Thomas Erick";
-var n1 = 30;
-var n2 = 20;
-var frase = "Japão é o maior país do mundo";
+//String
+
+// var nome = "Thomas Erick";
+// var n1 = 30;
+// var n2 = 20;
+// var frase = "Japão é o maior país do mundo";
 //alert(nome + "tem: " + n1 + " anos");
 //alert(n1 + n2);
-console.log(nome);
-console.log(n1 + n2);
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
-console.log(frase.replace("Japão", "Brasil"));
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
+// console.log(frase.replace("Japão", "Brasil"));
+
+//*Array
+
+// var lista = ["maça", "banana", "laranja"];
+// lista.push("pera");
+// lista.pop();
+
+// console.log(lista);
+// console.log(lista.reverse());
+
+// console.log(lista[0]);
+
+// console.log(lista.toString()[0]);
+// console.log(lista.join(" | "));
+
+//*Dicionário
+
+// var fruta = { nome: "maça", cor: "vermelha" };
+// console.log(fruta);
+// console.log(fruta.nome);
+
+//Lista de Dicionários
+
+var frutas = [
+  { nome: "maça", cor: "vermelha" },
+  { nome: "banana", cor: "amarela" }
+];
+console.log(frutas);
