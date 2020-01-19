edb699744e77326517a662390157d35e922631f3
Thomas <thominhaserick@gmail.com>
2020-01-19 16:01:03 -0300

    iniciando_app

---
+++ rocket_seat/app/main.js
@@ -0,0 +1,5 @@
+var listElement = document.querySelector("#app ul");
+var inpuElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = ["Fazer Café", "Estudar Js", "Acessar Comunidade"];
