ef4b819802983157f4d82d16f0de9af9f81ec044
Thomas <thominhaserick@gmail.com>
2020-01-19 12:47:32 -0300

    desafios

---
+++ rocket_seat/introducao/desafio.js
@@ -0,0 +1,83 @@
+// 1
+
+// var endereco = {
+//   rua: "Guia Lopes",
+//   numero: "393",
+//   bairro: "SantoAntonio",
+//   cidade: "Joinville",
+//   uf: "SC"
+// };
+
+// var result =
+//   "Usuario mora em " +
+//   endereco.cidade +
+//   "/" +
+//   endereco.uf +
+//   ", na rua " +
+//   endereco.rua +
+//   " com n° " +
+//   endereco.numero;
+
+// console.log(result);
+
+//2
+
+// function pares(n1, n2) {
+//   for (var i = n1; i <= n2; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+// pares(1, 100);
+
+//3
+
+// function skill(skills) {
+//   if (skills.indexOf("js1") === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// var skills = ["js", "ReactJs", "Flutter"];
+// console.log(skill(skills));
+
+//4
+
+// function experiencia(anos) {
+//   if (anos > 0 && anos <= 1) {
+//     console.log("Iniciante");
+//   } else if (anos >= 1 && anos <= 3) {
+//     console.log("Intermediarios");
+//   } else if (anos >= 3 && anos <= 6) {
+//     console.log("Avançado");
+//   } else if (anos >= 7) {
+//     console.log("Jedi Master");
+//   }
+// }
+
+// experiencia(7);
+
+//5
+// var usuarios = [
+//   {
+//     nome: "Thomas",
+//     habilidade: ["JS", "REACTJS", "JAVA"]
+//   },
+//   {
+//     nome: "Jaque",
+//     habilidade: ["PHP", "VueJS", "Pyton"]
+//   }
+// ];
+
+// for (var i = 0; i < usuarios.length; i++) {
+//   var result =
+//     "O " +
+//     usuarios[i].nome +
+//     " possui as habilidades: " +
+//     usuarios[i].habilidade.join("-");
+
+//   console.log(result);
+// }
