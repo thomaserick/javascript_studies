a3a951a61a50a1d6104f81cce1cf935a98ee17ab
Thomas <thominhaserick@gmail.com>
2020-01-19 13:22:34 -0300

    manipulando_element

--- rocket_seat/DOM/main.js
+++ rocket_seat/DOM/main.js
@@ -7,10 +7,23 @@
 //var inputElement = document.getElementsByClassName("name");
 //console.log(inputElement);
 
-var inputElement = document.querySelector("input[name=name]");
-var btnElement = document.querySelector("button.botao");
+// var inputElement = document.querySelector("input[name=name]");
+// var btnElement = document.querySelector("button.botao");
 
-btnElement.onclick = function() {
-  var text = inputElement.value;
-  alert(text);
-};
+// btnElement.onclick = function() {
+//   var text = inputElement.value;
+//   alert(text);
+// };
+
+//*Adiciona um Link
+
+var linkElement = document.createElement("a");
+linkElement.setAttribute("href", "http://www.google.com.br");
+linkElement.setAttribute("title", "Site da Google");
+var textElement = document.createTextNode("Acessar site Google");
+linkElement.appendChild(textElement);
+var containerElement = document.querySelector("#app");
+containerElement.appendChild(linkElement);
+
+var inputElement = document.querySelector("#name");
+containerElement.removeChild(inputElement);
