d0335ab8efa0d5e6aa8791685448bbbab8dfa07d
Thomas <thominhaserick@gmail.com>
2020-01-19 11:27:14 -0300

    variaveis

---
+++ rocket_seat/README.md
@@ -0,0 +1,12 @@
+# Curso RocketSeat
+
+## Introdução ao JavaScript
+
+### Aprendizado
+
+- Variaveis
+- Funções
+- Condicionais
+- Estruturas de Repetição
+- Requisções assincronas
+- construindo um app do zero
---
+++ rocket_seat/introducao/main.js
@@ -0,0 +1,15 @@
+var nome = "Thomas";
+var idade = 30;
+var pedo = 80;
+var humano = true;
+var alunos = ["Jaque", "Thomas", "Lucas"];
+
+var pessoa = {
+  nome: "Thomas",
+  idade: 30,
+  peso: 80,
+  humano: true
+};
+
+console.log(pessoa.nome);
+console.log(alunos[1]);
