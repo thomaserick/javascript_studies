4382fbee28e396901012a396b043254585bbe56b
Thomas <thominhaserick@gmail.com>
2020-01-19 16:24:21 -0300

    criando_todos

--- rocket_seat/app/main.js
+++ rocket_seat/app/main.js
@@ -3,3 +3,25 @@ var inpuElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
 var todos = ["Fazer Café", "Estudar Js", "Acessar Comunidade"];
+
+function renderTodos() {
+  listElement.innerHTML = "";
+
+  for (todo of todos) {
+    var todoElement = document.createElement("li");
+    var todoText = document.createTextNode(todo);
+    todoElement.appendChild(todoText);
+
+    listElement.appendChild(todoElement);
+  }
+}
+renderTodos();
+
+function addTodo() {
+  var todoText = inpuElement.value;
+  todos.push(todoText);
+  inpuElement.value = "";
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo;
