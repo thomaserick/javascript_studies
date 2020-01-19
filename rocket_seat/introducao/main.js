8adc7dccff2ca00e0a4b7008d9d5d7a283e2cf12
Thomas <thominhaserick@gmail.com>
2020-01-19 11:48:36 -0300

    operacoes

--- rocket_seat/introducao/main.js
+++ rocket_seat/introducao/main.js
@@ -1,15 +1,31 @@
-var nome = "Thomas";
-var idade = 30;
-var pedo = 80;
-var humano = true;
-var alunos = ["Jaque", "Thomas", "Lucas"];
+//* Variaveis
+// var nome = "Thomas";
+// var idade = 30;
+// var pedo = 80;
+// var humano = true;
+// var alunos = ["Jaque", "Thomas", "Lucas"];
 
-var pessoa = {
-  nome: "Thomas",
-  idade: 30,
-  peso: 80,
-  humano: true
-};
+// var pessoa = {
+//   nome: "Thomas",
+//   idade: 30,
+//   peso: 80,
+//   humano: true
+// };
 
-console.log(pessoa.nome);
-console.log(alunos[1]);
+// console.log(pessoa.nome);
+// console.log(alunos[1]);
+
+//*operações Matemáticas
+
+var x = 10,
+  y = 5;
+
+var sub = x + y;
+var mult = x * y;
+var div = x / y;
+var mod = x % y;
+
+x *= y;
+x = x + 3; //x +=3;
+
+console.log(x);
