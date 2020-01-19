e63724b62e2869d3749d377256b4bd9eb46ea821
Thomas <thominhaserick@gmail.com>
2020-01-19 11:49:05 -0300

    function

--- rocket_seat/introducao/main.js
+++ rocket_seat/introducao/main.js
@@ -17,15 +17,27 @@
 
 //*operações Matemáticas
 
-var x = 10,
-  y = 5;
+// var x = 10,
+//   y = 5;
 
-var sub = x + y;
-var mult = x * y;
-var div = x / y;
-var mod = x % y;
+// var sub = x + y;
+// var mult = x * y;
+// var div = x / y;
+// var mod = x % y;
 
-x *= y;
-x = x + 3; //x +=3;
+// x *= y;
+// x = x + 3; //x +=3;
 
-console.log(x);
+// console.log(x);
+
+//*Funçoes
+
+function soma(n1, n2) {
+  var result = n1 + n2;
+
+  return result;
+}
+
+var result = soma(1, 4);
+
+console.log(result);
