a4cfa82dae219c9d3b0d666132a20941a7d09033
Thomas <thominhaserick@gmail.com>
2020-01-19 13:13:30 -0300

    querySelector

--- rocket_seat/DOM/main.js
+++ rocket_seat/DOM/main.js
@@ -1,3 +1,16 @@
-function mostraAlert() {
-  alert("Botão Clicado");
-}
+// function mostraAlert() {
+//   alert("Botão Clicado");
+// }
+
+//var inputElement = document.getElementById("name");
+//var inputElement = document.getElementsByTagName("input");
+//var inputElement = document.getElementsByClassName("name");
+//console.log(inputElement);
+
+var inputElement = document.querySelector("input[name=name]");
+var btnElement = document.querySelector("button.botao");
+
+btnElement.onclick = function() {
+  var text = inputElement.value;
+  alert(text);
+};
