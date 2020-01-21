f42682205c7ae2c0538670c6093b50f9606264e1
Thomas <thominhaserick@gmail.com>
2020-01-21 12:54:12 -0300

    const e let

--- rocket_seat/ES6/main.js
+++ rocket_seat/ES6/main.js
@@ -1,29 +1,65 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
+// class List {
+//   constructor() {
+//     this.data = [];
+//   }
 
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
+//   add(data) {
+//     this.data.push(data);
+//     console.log(this.data);
+//   }
+// }
+
+// class TodoList extends List {
+//   constructor() {
+//     super();
+//     this.usuario = "Thomas";
+//   }
+
+//   mostraUsuario() {
+//     console.log(this.usuario);
+//   }
+// }
+
+// const MinhaLista = new TodoList();
+
+// document.getElementById("newtodo").onclick = function() {
+//   MinhaLista.add("Novo Todo");
+// };
+
+// MinhaLista.mostraUsuario();
+
+//Não funciona por ser statico
+// class TodosList {
+// constructor() {
+//     this.todos = [];
+// }
+
+//   static addTodo() {
+//     this.todos.push("Novo Todo");
+//     console.log(this.todos);
+//   }
+// }
+// TodosList.addTodo();
+
+// Funciona pois nao precisa de informção do restante da classe;
+// class Matematic {
+// static soma(a, b) {
+//     return a + b;
+// }
+// }
+// console.log(Matematic.soma(2, 5));
+
+//Const e Let
+
+const t = 30;
+
+function teste(x) {
+  let y = 2;
+
+  if (x > 5) {
+    let y = 4;
   }
+  console.log(x, y);
 }
 
-class TodoList extends List {
-  constructor() {
-    super();
-    this.usuario = "Thomas";
-  }
-
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-const MinhaLista = new TodoList();
-
-document.getElementById("newtodo").onclick = function() {
-  MinhaLista.add("Novo Todo");
-};
-
-MinhaLista.mostraUsuario();
+teste();
