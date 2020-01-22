278628488af03130f9315d9a1270cfdc6fd50ec0
Thomas <thominhaserick@gmail.com>
2020-01-22 12:00:44 -0300

    loading e error

--- rocket_seat/ES6/src/main.js
+++ rocket_seat/ES6/src/main.js
@@ -14,6 +14,17 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement("span");
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute("id", "loading");
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById("loading").remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -23,27 +34,35 @@ class App {
       return;
     }
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    //Desestruturação
-    const {
-      name,
-      description,
-      html_url,
-      owner: { avatar_url }
-    } = response.data;
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    console.log(response);
+      //Desestruturação
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url }
+      } = response.data;
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
+      //console.log(response);
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.render();
+    } catch (err) {
+      alert("o Repositorio não existe!");
+    }
 
     this.inputEl.value = "";
-    this.render();
+    this.setLoading(false);
   }
 
   render() {
