b40e533cac1f7975ecd32f9c14f11713b2cb18e8
Thomas <thominhaserick@gmail.com>
2020-03-10 23:25:57 -0300

    Constante com const

---
+++ Curso_avançado/basico/aula7/index.js
@@ -0,0 +1,11 @@
+//Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+//Não podem conter espaços ou traços
+//Case-sensitive
+//Não pode modificar o valor de uma constante
+//NÃO UTILIZE VAR, UTILIZE CONST
+//Nao pode ser alteradas ao longo do código
+const name = "Thomas";
+console.log(name);
+console.log(typeof name);
