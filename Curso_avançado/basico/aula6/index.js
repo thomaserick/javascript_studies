8046e9f75b62e32c777b763dde10c113838a4c2e
Thomas <thominhaserick@gmail.com>
2020-03-10 23:06:04 -0300

    Variaveis com let

---
+++ Curso_avançado/basico/aula6/index.js
@@ -0,0 +1,6 @@
+let name = "Joao";
+var name2 = "Variavel";
+
+console.log(name, "nasceu em 1984");
+console.log("Em 200 " + name + " conheceu Maria");
+console.log("Maria teve 1 filho com o", name);
