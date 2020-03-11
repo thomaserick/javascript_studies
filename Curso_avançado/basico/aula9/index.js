bd9649b217fe30af0cd7f2a538b677c1bbb2d60f
Thomas <thominhaserick@gmail.com>
2020-03-10 23:42:34 -0300

    Let vs Var

---
+++ Curso_avançado/basico/aula9/index.js
@@ -0,0 +1,16 @@
+//ECMAScript 2015 (ES6)
+//Var sobescreve o name
+var name = "Thomas";
+var name = "Jaque";
+
+console.log(name);
+
+//Redeclarar com Let nao é suportado
+let name2 = "Thomas";
+let name2 = "Jaque";
+
+console.log(name2);
+
+name3 = "Thomas Erick"; //NÃO É CORRETO cria uma variável global
+
+console.log(name3);
