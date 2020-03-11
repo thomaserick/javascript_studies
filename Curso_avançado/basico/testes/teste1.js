d7a012c641da7de7c4bec2ee7897f7ed15dbdd55
Thomas <thominhaserick@gmail.com>
2020-03-10 23:37:16 -0300

    teste de let e const

---
+++ Curso_avançado/basico/testes/teste1.js
@@ -0,0 +1,21 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+const nome = "Thomas Erick";
+const sobrenome = "Oliveira";
+const idade = 30;
+const peso = 80;
+const alturaEmM = 1.75;
+let indiceMassaCorporal; // peso / (altura * altura)
+let anoNascimento;
+
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2019 - idade;
+
+// Template Strings
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
