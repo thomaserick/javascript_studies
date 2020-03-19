ed9f7a31c4a23c3cb8c40c70e3d76450f70a4686
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-19 12:15:39 -0300

    logical operator

---
+++ Curso_avançado/logica_programacao/aula2/index.js
@@ -0,0 +1,15 @@
+/* 
+Operadores Lógicos
+*/
+
+//&&-> AND -> E
+
+console.log(true && true);
+
+//||->OR -> OU
+
+console.log(false || true || false);
+
+// ! -> NOT -> NÃO
+
+console.log(!false == false);
