03b34103a196eb0feac3c9a220a620b75a530500
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-20 18:29:16 -0300

    Switch

--- Curso_avançado/logica_programacao/aula8/index.js
+++ Curso_avançado/logica_programacao/aula8/index.js
@@ -1,31 +1,26 @@
 const data = new Date("1989-11-09 00:00:00");
 const diaSemana = data.getDay();
-let diaSemanaTexto;
-
-switch (diaSemana) {
-  case 0:
-    diaSemanaTexto = "Domingo";
-    break;
-  case 1:
-    diaSemanaTexto = "Segunda";
-    break;
-  case 2:
-    diaSemanaTexto = "Terça";
-    break;
-  case 3:
-    diaSemanaTexto = "Quarta";
-    break;
-  case 4:
-    diaSemanaTexto = "Quinta";
-    break;
-  case 5:
-    diaSemanaTexto = "Sexta";
-    break;
-  case 6:
-    diaSemanaTexto = "Sábado";
-    break;
-  default:
-    diaSemanaTexto = "";
-}
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
 
 console.log(diaSemanaTexto);
+
+function getDiaSemanaTexto(diaSemana) {
+  switch (diaSemana) {
+    case 0:
+      return "Domingo";
+    case 1:
+      return "Segunda";
+    case 2:
+      return "Terça";
+    case 3:
+      return "Quarta";
+    case 4:
+      return "Quinta";
+    case 5:
+      return "Sexta";
+    case 6:
+      return "Sábado";
+    default:
+      return "";
+  }
+}
