be7811c3f26b0825322d0b4838c6005f9286d994
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-21 11:15:20 -0300

    Desestruturacao Array

---
+++ Curso_avançado/logica_programacao/aula11/index.js
@@ -0,0 +1,11 @@
+//Atribuição via desestruturação
+// let a = "A";
+// let b = "B";
+// let c = "C";
+// const letras = [b, c, a];
+// [a, b, c] = letras;
+// console.log(a, b, c);
+
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const [a, b] = numeros;
+console.log(a, b);
