b2c5cc782ffe049985e8cdfeff6b3eb500242b0c
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-21 14:32:41 -0300

    While/Do While

---
+++ Curso_avançado/logica_programacao/aula18/index.js
@@ -0,0 +1,23 @@
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+function random(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return Math.floor(r);
+}
+
+let rand = random(1, 50);
+
+while (rand !== 10) {
+  console.log(rand);
+  let rand = random(1, 50);
+}
+
+do {
+  let rand = random(1, 50);
+  console.log(rand);
+} while (rand != 10);
