ee205b721c85a29c58fd0742f7440ef6bf3092ba
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-21 15:58:06 -0300

    SetInterval/SetTimeout

---
+++ Curso_avançado/logica_programacao/aula21/index.js
@@ -0,0 +1,12 @@
+function mostraHora() {
+  let data = new Date();
+  return data.toLocaleTimeString("pt-BR", { hour12: false });
+}
+
+const timer = setInterval(function() {
+  console.log(mostraHora());
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(timer);
+}, 10000);
