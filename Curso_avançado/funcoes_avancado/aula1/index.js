60210474638d18cfc7f621d00c81ae72f829f33a
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-22 20:31:18 -0300

    Declaring function

---
+++ Curso_avançado/funcoes_avancado/aula1/index.js
@@ -0,0 +1,33 @@
+//Declaracao de função (Funcstion hoisting) -> Eleva as declarações no topo as VAR
+
+falaOi();
+
+function falaOi() {
+  console.log("Oie");
+}
+
+// Fist-class object (objetos de primeira Classe)
+//Function expression
+//pode ser tratada como dado
+
+const souUmDado = () => {
+  console.log("Sou um dado");
+};
+
+souUmDado();
+
+// Arrow Function
+const arrow = () => {
+  console.log("Sou uma arrow function");
+};
+
+arrow();
+
+//Dentro de um Objeto
+const obj = {
+  falar: function() {
+    console.log("Doidera");
+  }
+};
+
+obj.falar();
