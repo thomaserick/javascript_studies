5c2addb905c485921fbf36aa7160ffddd3fab4b6
Thomas Erick <thomas.oliveira@neomind.com.br>
2020-03-24 21:09:56 -0300

    generation functions

---
+++ Curso_avançado/funcoes_avancado/aula12/index.js
@@ -0,0 +1,17 @@
+//Função geradoras
+
+function* geradora1() {
+  //Código qualquer
+  yield "Valor 1";
+
+  //Código qualquer
+  yield "Valor 2";
+
+  //Código qualquer
+  yield "Valor 3";
+}
+
+const g1 = geradora1();
+console.log(g1.next().value);
+console.log(g1.next().value);
+console.log(g1.next().value);
