a023aa90ad37c88de02e3df52cc31d5560cd5bb3
Thomas Erick <thominhaserick@gmail.com>
2020-05-13 22:24:22 -0300

    Chain Middlewares

---
+++ appFinancas/FundamentosMEAN/express/ex02.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res, next) => {
+  console.log("Inicio...");
+  next();
+  console.log("Fim..");
+});
+
+server.get("/", (req, res, next) => {
+  console.log("Resposta...");
+  next();
+  res.send("<h1>Olá Express!</h1>");
+});
+
+server.listen(3000, () => {
+  console.log(`Executando...`);
+});
