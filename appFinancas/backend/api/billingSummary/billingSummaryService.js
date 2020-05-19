20fde59cdc18a9fd67444df309eb9548ab88237f
Thomas Erick <thominhaserick@gmail.com>
2020-05-18 22:52:22 -0300

    Route billingSummaryService

--- appFinancas/backend/api/billingSummary/billingSummaryService.js
+++ appFinancas/backend/api/billingSummary/billingSummaryService.js
@@ -1,25 +1,27 @@
 const _ = require("lodash");
 const BillingCycle = require("../billingCycle/billingCycle");
 
-//Mais uma função middleware
+// Mais uma função middleware
 function getSummary(req, res) {
   BillingCycle.aggregate(
-    {
-      $project: {
-        credit: { $sum: "#credits.value" },
-        debt: { $sum: "debts.value" },
+    [
+      {
+        $project: {
+          credit: { $sum: "$credits.value" },
+          debt: { $sum: "$debts.value" },
+        },
       },
-      $group: {
-        _id: null,
-        credit: { $sum: "$credit" },
-        debt: { $sum: "$debt" },
+      {
+        $group: {
+          _id: null,
+          credit: { $sum: "$credit" },
+          debt: { $sum: "$debt" },
+        },
       },
-      $project: {
-        id: 0,
-        credit: 1,
-        debt: 1,
+      {
+        $project: { _id: 0, credit: 1, debt: 1 },
       },
-    },
+    ],
     function (error, result) {
       if (error) {
         res.status(500).json({ errors: [error] });
