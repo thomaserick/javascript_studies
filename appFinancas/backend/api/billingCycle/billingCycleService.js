5406b5bf863424035dc3651d46805e4c2ae7aa43
Thomas Erick <thominhaserick@gmail.com>
2020-05-18 22:11:35 -0300

    Service Count BillingCycles

--- appFinancas/backend/api/billingCycle/billingCycleService.js
+++ appFinancas/backend/api/billingCycle/billingCycleService.js
@@ -5,4 +5,17 @@ BillingCycle.methods(["get", "post", "put", "delete"]);
 //Retorna o objeto novo no update
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+BillingCycle.route("count", function (req, res) {
+  //Consulta
+  BillingCycle.count(function (error, value) {
+    if (error) {
+      res.status(500).json({
+        errors: [error],
+      });
+    } else {
+      res.json({ value });
+    }
+  });
+});
+
 module.exports = BillingCycle;
