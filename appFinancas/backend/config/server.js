e944f48a21b1ea9a9a97faaa70c5be2bfe41b1bc
Thomas Erick <thominhaserick@gmail.com>
2020-05-19 22:31:30 -0300

    Adjust Cors

--- appFinancas/backend/config/server.js
+++ appFinancas/backend/config/server.js
@@ -3,12 +3,14 @@ const port = 3003;
 const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
+const allowsCors = require("./cors");
 
 //Será capaz mais tipos de informações -> responsavel por fazer a interpretação
 //toda requisição
 server.use(bodyParser.urlencoded({ extended: true }));
 //Transforma em objeto
 server.use(bodyParser.json());
+server.use(allowsCors);
 
 server.listen(port, function () {
   console.log(`Backend is running on port ${port}.`);
