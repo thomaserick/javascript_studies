4ede0330fe7cedc98105287afafda1caf941f217
Thomas Erick <thominhaserick@gmail.com>
2020-05-21 22:36:31 -0300

    List billingCycle

--- appFinancas/frontend/app/billingCycle/billingCycleController.js
+++ appFinancas/frontend/app/billingCycle/billingCycleController.js
@@ -5,19 +5,27 @@
 
   function BillingCycleController($http, msgs) {
     const _self = this;
+    const url = `http://localhost:3003/api/billingCycles`;
 
     _self.createBillingCycle = function () {
-      const url = `http://localhost:3003/api/billingCycles`;
       $http
         .post(url, _self.billingCycle)
         .then(function (response) {
-          _self.billingCycle = {};
+          _self.refresh();
           msgs.addSuccess("Operação realizada com sucesso");
         })
         .catch(function (resp) {
-          console.log(resp);
           msgs.addError(resp.data.errors);
         });
     };
+
+    _self.refresh = function () {
+      $http.get(url).then(function (response) {
+        _self.billingCycle = {};
+        _self.billingCycles = response.data;
+      });
+    };
+
+    _self.refresh();
   }
 })();
