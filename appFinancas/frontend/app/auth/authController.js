319ecb41e9bc6354f69f8dcf9e5eee7b18a6cadd
Thomas Erick <thominhaserick@gmail.com>
2020-05-28 20:52:26 -0300

    Login/Register User

--- appFinancas/frontend/app/auth/authController.js
+++ appFinancas/frontend/app/auth/authController.js
@@ -5,6 +5,17 @@
 
   function AuthController($location, msgs) {
     const _self = this;
+    _self.loginMode = true;
+
+    _self.changeMode = () => (_self.loginMode = !_self.loginMode);
+
+    _self.login = () => {
+      console.log(`Login.... ${_self.user.email}`);
+    };
+
+    _self.signup = () => {
+      console.log(`Signup... ${_self.user.email}`);
+    };
 
     _self.getUser = () => ({
       name: "Thomas Erick Mock",
